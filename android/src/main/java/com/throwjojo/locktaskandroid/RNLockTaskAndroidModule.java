196ad96b9911a1b757ccfd336619699c4faf85a9
Jordan Holland <thisrememo@gmail.com>
2017-05-22 11:38:15 +1000

    Added Android module methods.

--- android/src/main/java/com/throwjojo/locktaskandroid/RNLockTaskAndroidModule.java
+++ android/src/main/java/com/throwjojo/locktaskandroid/RNLockTaskAndroidModule.java
@@ -1,6 +1,10 @@
 
 package com.throwjojo.locktaskandroid;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Build;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -15,9 +19,35 @@ public class RNLockTaskAndroidModule extends ReactContextBaseJavaModule {
     this.reactContext = reactContext;
   }
 
+  // Starts lock task mode
+  @ReactMethod
+  public void startLockTask() {
+    if (getCurrentActivity() != null) {
+      getCurrentActivity().startLockTask();
+    }
+  }
+
+  // Stops lock task mode
+  @ReactMethod
+  public void stopLockTask() {
+    if (getCurrentActivity() != null) {
+      getCurrentActivity().stopLockTask();
+    }
+  }
+
+  // Checks if lock task mode is active or not
+  private boolean isLockTaskModeActive() {
+    ActivityManager activityManager = (ActivityManager) reactContext.getSystemService(Context.ACTIVITY_SERVICE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return (activityManager.getLockTaskModeState() != ActivityManager.LOCK_TASK_MODE_NONE);
+    } else {
+      return activityManager.isInLockTaskMode();
+    }
+  }
+
   @Override
   public String getName() {
-    return "RNLockTaskAndroid";
+    return "LockTaskAndroid";
   }
 
 }
